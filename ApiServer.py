11b0352999ea31f0fa9073f1722eaf49e602e9fe
chienmy <qian.meiyuan@outlook.com>
2022-05-04 14:55:16 +0800

    Add: 增加韵部检查接口

--- ApiServer.py
+++ ApiServer.py
@@ -62,27 +62,78 @@ def check_ping_ze():
     yun_book = req_data["book"]
     pu = req_data["pu"]
     content = req_data["content"]
+    strict_flag = bool(req_data["strictMode"])
     result = []
     for i in range(len(content)):
+        pu_flag = pu[i]
+        # 严格模式
+        if strict_flag:
+            if pu_flag == "2":
+                pu_flag = "0"
+            if pu_flag == "3":
+                pu_flag = "1"
         if content[i] == " ":
             result.append(-1)
-        elif pu[i] in list("23"):
+        elif pu_flag in list("23"):
             result.append(1)
         else:
             ping_ze_truth = _get_ping_ze_flag(content[i], yun_book)
             if ping_ze_truth == -1 or ping_ze_truth == 2:
                 result.append(ping_ze_truth)
-            elif pu[i] == "0" or ord("a") <= ord(pu[i]) <= ord("z"):
+            elif pu_flag == "0" or ord("a") <= ord(pu_flag) <= ord("z"):
                 result.append(1 if ping_ze_truth == 0 else 0)
-            elif pu[i] == "1" or ord("A") <= ord(pu[i]) <= ord("Z"):
+            elif pu_flag == "1" or ord("A") <= ord(pu_flag) <= ord("Z"):
                 result.append(1 if ping_ze_truth == 1 else 0)
     return json.dumps(result, ensure_ascii=False)
 
 
+@app.route("/yun/check", methods=["POST"])
+def check_yun():
+    """
+    检查韵字是否符合
+    -1: 未知情况；0 - 不符合；1 - 符合；2 - 不确定
+    """
+    req_data = json.loads(request.get_data(), encoding="utf-8")
+    yun_book = req_data["book"]
+    yun_list = req_data["yun"]
+    yun_group = req_data["yunGroup"]
+    all_result = []
+    for i in range(len(yun_list)):
+        # 本组韵的校验结果
+        result = [-1 for _ in range(len(yun_group[i]))]
+        # 本组韵可能的韵部
+        possible_yun = []
+        for c in yun_group[i]:
+            if len(c) != 0:
+                possible_yun.append(set([y.part for y in Yun.select().where((Yun.value == c) & (Yun.book == yun_book))]))
+        # 仅有单字
+        if len(possible_yun) == 1:
+            # 该字一定符合
+            for j, c in enumerate(yun_group[i]):
+                if len(c) != 0:
+                    result[j] = 1
+        # 多字
+        elif len(possible_yun) > 1:
+            yun_set = possible_yun[0].intersection(*possible_yun[1:])
+            for j, c in enumerate(yun_group[i]):
+                if len(c) != 0:
+                    # 交集为空，每个字都为不符合状态
+                    if len(yun_set) == 0:
+                        result[j] = 0
+                    # 交集不为空，单韵部为符合，多韵部为不确定
+                    else:
+                        if Yun.select().where((Yun.value == c) & (Yun.book == yun_book)).count() == 1:
+                            result[j] = 1
+                        else:
+                            result[j] = 2
+        all_result.append(result)
+    return json.dumps(all_result, ensure_ascii=False)
+
+
 def _get_ping_ze_flag(c: str, yun_book: str):
     """
-    :params c: 待查询字符
-    :params yun_book: 韵书
+    :param c: 待查询字符
+    :param yun_book: 韵书
     :return -1 无字；0 平；1 仄；2 皆可
     """
     ping_ze_flag = -1
